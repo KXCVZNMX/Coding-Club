f177ab4574b492a732d427db5f3824da22654494
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2025-07-24 21:45:01 +1000

    Update: Removed redundant elements of `App`

--- classes.py
+++ classes.py
@@ -3,7 +3,6 @@ import pygame
 class App:
     def __init__(self):
         self._running = True
-        self._display_surface = None
         self.size = (self.width, self.height) = (800, 500)
         self.window = pygame.display.set_mode(self.size)
         self.background_color = (0, 0, 0)
@@ -11,16 +10,12 @@ class App:
         self.pos = [400, 490]
         self.speed = 5
         self.rect_size = (self.rect_width, self.rect_height) = (50, 50)
-        self.centre = (self.centre_x, self.centre_y) = (self.pos[0] + self.rect_width / 2, self.pos[1] + self.rect_height / 2)
 
     def is_running(self) -> bool:
         return self._running
 
     def stop_running(self):
         self._running = False
-
-    def update_centre(self):
-        self.centre = (self.pos[0] + self.rect_width / 2, self.pos[1] + self.rect_height / 2)
 
 
 class Block(pygame.sprite.Sprite):
